a76b23f0e77a9ea4c674cd44b32e4b298fa08c78
Pontus Karlsson <pontus@sent.com>
2017-11-27 06:33:48 +0530

    started no 19

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,40 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    .then(localMediaStream => {
+      console.log(localMediaStream);
+      video.src = window.URL.createObjectURL(localMediaStream);
+      video.play();
+    })
+    .catch(err => {
+      console.error('error', err);
+    });
+}
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 16);
+}
+
+function takePhoto() {
+  // Play the sound
+  snap.currentTime = 0;
+  snap.play();
+
+  // Take the data out of the canvas
+  const data = canvas.toDataURL('image/jpeg');
+}
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
